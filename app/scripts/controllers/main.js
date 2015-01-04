1c2f4f218fb05870111b60273f7ff1f958e3f610
Marco Marchese <marco.marchese75@gmail.com>
2015-01-05 00:24:23 +0100

    Versione 1.1
    Completata la serie di chiamate REST CRUD

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,97 @@ angular.module('restFeApp')
     $scope.id = null;
 
     $scope.allPatientsData = MainSrvc.query();
+    $scope.hash = "";
+    var payload = {};
 
     console.log(JSON.stringify($scope.allPatientsData));
+
+    $scope.insertedNewRecord = false;
+    $scope.createOrUpdate = function () {
+      var payload = {
+        "ass_cogn": $scope.ass_cogn,
+        "ass_nome": $scope.ass_nome,
+        "ass_tel": $scope.ass_tel,
+        "ass_email": $scope.ass_email
+      };
+      if($scope.ass_ipca != null)
+        payload.ass_ipca= $scope.ass_ipca;
+
+      console.log(JSON.stringify(payload));
+
+      $scope.allPatientsData.push(payload);
+      $scope.ass_email = null;
+      $scope.ass_nome = null;
+      $scope.ass_cogn = null;
+      $scope.ass_tel = null;
+
+      console.log(JSON.stringify($scope.allPatientsData));
+    }
+
+    $scope.saveOnDB = function () {
+      MainSrvc.postData({},  $scope.allPatientsData,
+        function (data) {
+          $scope.allPatientsData = MainSrvc.query();
+          $scope.insertedNewRecord = true;
+
+          $timeout(function () {
+            $scope.insertedNewRecord = false;
+          }, 5000);
+        }
+      );
+    }
+
+    $scope.deletedRecord = false;
+    $scope.removeItem = function (id) {
+      MainSrvc.remove({id: id}, function(){
+        $scope.deletedRecord = true;
+        $scope.allPatientsData = MainSrvc.query();
+        $timeout(function () {
+          $scope.deletedRecord = false;
+        }, 5000);
+      }, function(){
+        $scope.deletedRecord = false;
+        $scope.allPatientsData = MainSrvc.query();
+      });
+    }
+
+    $scope.editItem = function (data) {
+      var payload = {
+        "ass_ipca": data.ass_ipca,
+        "ass_cogn": data.ass_cogn,
+        "ass_nome": data.ass_nome,
+        "ass_tel": data.ass_tel,
+        "ass_email": data.ass_email
+      };
+      $scope.ass_ipca = data.ass_ipca;
+      $scope.hash =  data.$$hashKey;
+      $scope.editRecord = true;
+      $scope.ass_cogn = data.ass_cogn;
+      $scope.ass_nome = data.ass_nome;
+      $scope.ass_tel= data.ass_tel;
+      $scope.ass_email=data.ass_email;
+
+      console.log("HASH:" + $scope.hash);
+      console.log(JSON.stringify($scope.allPatientsData));
+
+      $scope.editRecord = false;
+      $scope.edit = function () {
+        //http://stackoverflow.com/questions/8668174/indexof-method-in-an-object-array
+        var i = arrayObjectIndexOf($scope.allPatientsData, $scope.hash, "$$hashKey");
+        console.log("Indice: "+ i);
+        $scope.allPatientsData.splice(i, 1); //rimuovo l'elemento
+        $scope.createOrUpdate(); //lo aggiungo ex-novo
+        $scope.editRecord = false;
+        $scope.ass_ipca = null;
+      }
+
+      function arrayObjectIndexOf(myArray, searchTerm, property) {
+        for(var i = 0, len = myArray.length; i < len; i++) {
+          if (myArray[i][property] === searchTerm) return i;
+        }
+        return -1;
+      }
+
+    }
 
   });
