b762ad512ea6f60105cc86f4defed605479df3b7
Marco Marchese <marco.marchese75@gmail.com>
2015-01-07 23:35:07 +0100

    Versione 1.4.2
    Commit con esperimenti su Bootstrap e direttive AngularJS.
    Tentativo di utilizzo UI Bootstrap.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngTable'
+    'ngTable' 
   ])
   .config(function ($routeProvider) {
     $routeProvider
---
+++ app/scripts/controllers/dropdownCtrl.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restFeApp.controller:DropdownCtrl
+ * @description
+ * # DropdownCtrl
+ * Controller of the restFeApp
+ */
+angular.module('restFeApp').controller('DropdownCtrl', function ($scope, $log) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+});
